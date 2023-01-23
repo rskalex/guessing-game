15e330c4782bc308929b9f08f981fdf299aabc0b
Alex Rusak <dobrovolya48@gmail.com>
2023-01-23 16:02:45 +0300

    feat: task done

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,20 +1,25 @@
 class GuessingGame {
-    constructor() {}
+    constructor() {
+        // this.min = 0,
+        // this.max = 0
+    }
 
     setRange(min, max) {
-
+        this.min = min,
+        this.max = max
     }
 
     guess() {
-
+        this.player = Math.round((this.max + this.min) / 2)
+        return this.player
     }
 
     lower() {
-
+        this.max = this.player
     }
 
     greater() {
-
+        this.min = this.player
     }
 }
 
